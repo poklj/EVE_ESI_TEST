f097424b4ad289c259767dccf680fcaddb7186e5
poklj <compgeek223@gmail.com>
2019-06-18 23:57:15 -0300

    Start working on the ESI Callback side of the SSO, Currently need to start working using XAMMP, this currently 502's

---
+++ AuthCallback.php
@@ -0,0 +1,43 @@
+<?php
+
+class AuthCallback
+{
+    /**
+     * Callback function for ESI Authenticating
+     */
+    function Callback(){
+        $f = fopen("../res/SecretKey.txt", "r");
+        $secretkey = fread($f, filesize("../res/SecretKey.txt"));
+        fclose($f);
+
+        $f = fopen("../res/esi_id.txt", "r");
+        $esi_id = fread($f, filesize("../res/esi_id.txt"));
+        close($f);
+
+        $url = "https://login.eveonline.com/v2/oauth/token";
+        $data = array('grant_type' => "authorization_code", "code" => $_GET["code"]);
+        $options = array(
+          'http' => array(
+              "method" => "POST",
+              "Authorization" => "Basic" . base64_encode($esi_id . + ":" . $secretkey),
+              "Content-Type" => "application/x-www-form-urlencoded",
+              "Host" => "login.eveonline.com",
+              $data
+          )
+        );
+        $context = stream_context_create($options);
+
+        $response = file_get_contents($url, false, $context);
+
+        if ($response = false) { //ERROR
+            echo "Eve SSO failed to respond, ESI might be down?";
+            return;
+        }
+        var_dump($response);
+        return;
+
+    }
+}
+
+$client = new AuthCallback();
+$client->Callback();
--- script/ESI_AUTH.js
+++ script/ESI_AUTH.js
@@ -2,12 +2,11 @@
 
 let SSO = {
     response_type: "code",
-    redirect_uri:"http%3A%2F%2Flocalhost%3A63342%2FESI_Test%2Fcallback.html",
+    redirect_uri:"http%3A%2F%2Flocalhost%3A63342%2FESI_Test%2FAuthCallback.php",
     client_id: "041ea789d4ed4acf8197f9f989daa51e",
     scope: "publicData%20esi-calendar.respond_calendar_events.v1%20esi-calendar.read_calendar_events.v1%20esi-location.read_location.v1%20esi-location.read_ship_type.v1%20esi-mail.organize_mail.v1%20esi-mail.read_mail.v1%20esi-mail.send_mail.v1%20esi-skills.read_skills.v1%20esi-skills.read_skillqueue.v1%20esi-wallet.read_character_wallet.v1%20esi-wallet.read_corporation_wallet.v1%20esi-search.search_structures.v1%20esi-clones.read_clones.v1%20esi-characters.read_contacts.v1%20esi-universe.read_structures.v1%20esi-bookmarks.read_character_bookmarks.v1%20esi-killmails.read_killmails.v1%20esi-corporations.read_corporation_membership.v1%20esi-assets.read_assets.v1%20esi-planets.manage_planets.v1%20esi-fleets.read_fleet.v1%20esi-fleets.write_fleet.v1%20esi-ui.open_window.v1%20esi-ui.write_waypoint.v1%20esi-characters.write_contacts.v1%20esi-fittings.read_fittings.v1%20esi-fittings.write_fittings.v1%20esi-markets.structure_markets.v1%20esi-corporations.read_structures.v1%20esi-characters.read_loyalty.v1%20esi-characters.read_opportunities.v1%20esi-characters.read_chat_channels.v1%20esi-characters.read_medals.v1%20esi-characters.read_standings.v1%20esi-characters.read_agents_research.v1%20esi-industry.read_character_jobs.v1%20esi-markets.read_character_orders.v1%20esi-characters.read_blueprints.v1%20esi-characters.read_corporation_roles.v1%20esi-location.read_online.v1%20esi-contracts.read_character_contracts.v1%20esi-clones.read_implants.v1%20esi-characters.read_fatigue.v1%20esi-killmails.read_corporation_killmails.v1%20esi-corporations.track_members.v1%20esi-wallet.read_corporation_wallets.v1%20esi-characters.read_notifications.v1%20esi-corporations.read_divisions.v1%20esi-corporations.read_contacts.v1%20esi-assets.read_corporation_assets.v1%20esi-corporations.read_titles.v1%20esi-corporations.read_blueprints.v1%20esi-bookmarks.read_corporation_bookmarks.v1%20esi-contracts.read_corporation_contracts.v1%20esi-corporations.read_standings.v1%20esi-corporations.read_starbases.v1%20esi-industry.read_corporation_jobs.v1%20esi-markets.read_corporation_orders.v1%20esi-corporations.read_container_logs.v1%20esi-industry.read_character_mining.v1%20esi-industry.read_corporation_mining.v1%20esi-planets.read_customs_offices.v1%20esi-corporations.read_facilities.v1%20esi-corporations.read_medals.v1%20esi-characters.read_titles.v1%20esi-alliances.read_contacts.v1%20esi-characters.read_fw_stats.v1%20esi-corporations.read_fw_stats.v1%20esi-characterstats.read.v1",
     state: "yeet"
 };
-
 
 /*
         Holdup Functions
@@ -50,10 +49,12 @@ function redirectSSO(){
     //shunt user to SSO
     window.location.replace(uri);
 }
+/*
 
+ TODO: Remove this once I get the AuthCallback.php working
 function callbackSSO(){
     let authcode = getQueryVariable("code");
-    let pre = readTextFile("file://../res/esi_id.txt") + ":" + readTextFile("file://../res/SecretKey.txt")
+    let pre = readTextFile("file://../res/esi_id.txt") + ":" + readTextFile("file://../res/SecretKey.txt");
     let Secret = btoa(pre);
     let uri = "https://login.eveonline.com/v2/oauth/token";
 
@@ -81,3 +82,5 @@ function callbackSSO(){
     }
     request.send(postData);
 }
+
+*/
